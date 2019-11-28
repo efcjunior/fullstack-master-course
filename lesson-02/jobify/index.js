7ec8d01ee283d00c0fa60502ccac3cd0dda05fd1
efcjunior <everson.c.junior@gmail.com>
2019-11-27 23:49:26 -0300

    fixing path

--- lesson-02/jobify/index.js
+++ lesson-02/jobify/index.js
@@ -8,6 +8,7 @@ const dbConnection = sqlite.open(path.resolve(__dirname, 'database.sqlite'), {Pr
 
 const port = process.env.PORT || 3000
 
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended:true}))
